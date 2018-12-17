017999028d2178eb8c642f0904968da00e2337e4
iSmash <nemeth.richo@gmail.com>
2018-12-17 13:37:14 +0000

    problem 12 Highly divisible triangular number

---
+++ problem12_highly_dev_triangular.rb
@@ -0,0 +1,28 @@
+#slow with basic methods. Need to be recoded
+
+a = 12375
+final = 1
+dev = 1
+temp_num = 0
+temp_array = []
+
+while temp_array.length <= 501
+ final = ((a + 1 ) * a) / 2
+  dev = 1
+  temp_array = []
+
+  while final >= dev
+      temp_array << dev if (final % dev) == 0
+   dev +=1 
+  end
+ p final
+ p a
+ a += 1
+end
+ 
+p "The number of checked triangle numbers"
+p (a - 1)
+puts
+p "The answer is:"
+p final
+
